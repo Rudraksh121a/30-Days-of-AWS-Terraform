737016ff13ea55d2126afad2c16bf0340615f344
Rudraksh Kanungo <141481238+Rudraksh121a@users.noreply.github.com>
2025-12-02 00:13:44 +0530

    Refactor AWS provider and S3 bucket settings
    Updated AWS provider configuration and S3 bucket name.

--- Day3/main.tf
+++ Day3/main.tf
@@ -1,21 +1,15 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.23.0"
+    }
+  }
 }
 
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-demo-bucket-${aws_vpc.main_vpc.id}"  
-
-  tags = {
-    Name = "example-bucket"
-    VPC  = aws_vpc.main_vpc.id
-  }
+  bucket = "my-demo-bucket-rudra121a" 
 }
