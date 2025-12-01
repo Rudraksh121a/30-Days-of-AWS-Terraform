0ce2a7e396204c7c62d39279419aec9558c880ad
Rudraksh Kanungo <141481238+Rudraksh121a@users.noreply.github.com>
2025-12-01 13:37:46 +0530

    Configure AWS provider and create S3 bucket resources
    Added AWS provider and S3 bucket resources for state management.

--- Day4/main.tf
+++ Day4/main.tf
@@ -1,9 +1,36 @@
 terraform {
-  backend "s3" {
-    bucket       = "my-terraform-state"
-    key          = "dev/terraform.tfstate"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "myDevBucket" 
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+
